6ac22c2c2b47266535ddf8e13ad63acbcd9adaf7
leonelcolina <tang.leonel@gmail.com>
2022-06-20 08:44:20 -0400

    w5d1 first commit

--- w4d5/bigo.rb
+++ w4d5/bigo.rb
@@ -3,20 +3,22 @@ a=['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh',
 
 
  def sluggish_octopus(array)
-
     fish=""
-
     array.each_with_index do |ele1, idx1|
         array.each_with_index do |ele2,idx2|
-
             ele1.length > ele2.length ? fish=ele1 : fish=ele2
-
         end
     end
-
-
     fish
  end
 
+ def dominant_octopus(arr)
 
- p sluggish_octopus(a)
+    first=arr[0]
+
+    left= arr[1..-1].select{|ele| ele.length < first.length}
+
+
+
+
+ end
