342cab0d7de1b461bced77a2d097b5c45b722e97
leonelcolina <tang.leonel@gmail.com>
2022-07-06 09:30:44 -0400

    finish w7d2 homework

--- w5d4/app/models/house.rb
+++ w5d4/app/models/house.rb
@@ -1,10 +1,10 @@
 class House < ApplicationRecord
-  # validates :address, presence: true
+  validates :address, presence: true
 
   has_many(
     :people,
-    class_name: 'Person',
-    foreign_key: 'house_id',
-    primary_key: ids
+    class_name: :Person,
+    foreign_key: :house_id,
+    primary_key: :id
   )
 end
---
+++ w7d2/helper_methods_99cats/app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+    default from: 'everybody@appacademy.io'
+
+
+    def welcome_email(user)
+        # your code here
+        Welcome, brp
+    end
+end
